06b8fa1c0ce514cf6b1b0d28be217eac1658ae0e
mattander91 <mattander91@gmail.com>
2018-04-19 16:11:20 -0700

    test

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let port = process.env.port || 3001;
+let port = process.env.PORT || 3001;
 
 
 //gets users tried beers and wishlist beers
