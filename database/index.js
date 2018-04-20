492f3b533c9945341b19a55eaa883e7a5f74fbe8
mattander91 <mattander91@gmail.com>
2018-04-19 18:16:37 -0700

    configured DB for mLab

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-let DB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/beers';
+let DB_URI = process.env.MONGODB_URI;
 
 mongoose.connect(DB_URI, { useMongoClient: true });
 
