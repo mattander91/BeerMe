f414422a087bcef0f1f07a811294fec236d1871e
mattander91 <mattander91@gmail.com>
2018-04-23 12:57:34 -0700

    Refactored helpers.js

--- server/helpers.js
+++ server/helpers.js
@@ -8,30 +8,7 @@ const brewdb = new BreweryDb(apiKey);
 let getBeerData = (search, callback) => {
   brewdb.search.beers({q: search, withBreweries: 'Y'}, (err, data) => {
     if (data) {
-      let beerData = [];
-      for (let i = 0; i < data.length; i++) {
-        let icon = '';
-        let brewIcon = '';
-        let styleDescription = '';
-        data[i].labels ? icon = data[i].labels.medium : icon = 'img/beer-image.jpg';
-        data[i].breweries[0].images ? brewIcon = data[i].breweries[0].images.medium : brewIcon = null;
-        data[i].style ? styleDescription = data[i].style.description : styleDescription = '';
-        let relevance = addRelevance(search, data[i].name, data[i].breweries[0].name);
-        if (relevance > 0) {
-          beerData.push({
-            name: data[i].name,
-            id: data[i].id,
-            des: data[i].description || styleDescription,
-            icon: icon,
-            abv: data[i].abv,
-            ibu: data[i].ibu,
-            brewer: data[i].breweries[0].name,
-            brewerIcon: brewIcon,
-            relevance: relevance
-          });
-        }
-      }
-      callback(beerData);
+      callback(filterData(data, search, true));
     } else if (err) {
       // console.log('error getting data: ', err);
       callback([{noData: true}]);
@@ -39,6 +16,48 @@ let getBeerData = (search, callback) => {
       callback([{noData: true}]);
     }
   });
+}
+
+let getBeerId = (ids, params, callback) => {
+  let beerData = [];
+  if (ids.length > 0) {
+    brewdb.beer.getById(ids, params, (err, data) => {
+      if (data) {
+        callback(filterData(data));
+      }
+      else if (err) {
+        // console.log('error getting ids: ', err);
+        callback([{noData: true}]);
+      }
+    });
+  } else {
+    callback([{noData: true}]);
+  }
+};
+
+//filters out data for searching beers and user's lists
+let filterData = (data, search, rel) => {
+  let beerData = [];
+  let relevance = null;
+  for (let i = 0; i < data.length; i++) {
+    if (rel) {
+      relevance = addRelevance(search, data[i].name, data[i].breweries[0].name);
+    }
+    if (!rel || relevance > 0) {
+      beerData.push({
+        name: data[i].name,
+        id: data[i].id,
+        des: data[i].description || data[i].style ? data[i].style.description : '',
+        icon: data[i].labels ? data[i].labels.medium : 'img/beer-image.jpg',
+        abv: data[i].abv,
+        ibu: data[i].ibu,
+        brewer: data[i].breweries[0].name,
+        brewerIcon: data[i].breweries[0].images ? data[i].breweries[0].images.medium : null,
+        relevance: relevance
+      });
+    }
+  }
+  return beerData;
 }
 
  //Aadds relevance each time a users search word(s) is found in the combined brewery + beer name
@@ -57,41 +76,7 @@ let addRelevance = (search, beerName, brewer) => {
   return relevance;
 };
 
-let getBeerId = (ids, params, callback) => {
-  let beerData = [];
-  if (ids.length > 0) {
-    brewdb.beer.getById(ids, params, (err, data) => {
-      if (data) {
-        for (let i = 0; i < data.length; i++) {
-          let icon = '';
-          let brewIcon = '';
-          let styleDescription = '';
-          data[i].labels ? icon = data[i].labels.medium : icon = 'img/beer-image.jpg';
-          data[i].breweries[0].images ? brewIcon = data[i].breweries[0].images.medium : brewIcon = null;
-          data[i].style ? styleDescription = data[i].style.description : styleDescription = '';
-          beerData.push({
-            name: data[i].name,
-            id: data[i].id,
-            des: data[i].description || styleDescription,
-            icon: icon,
-            abv: data[i].abv,
-            ibu: data[i].ibu,
-            brewer: data[i].breweries[0].name || 'brewer not available',
-            brewerIcon: brewIcon
-          });
-        }
-        callback(beerData);
-      }
-      else if (err) {
-        // console.log('error getting ids: ', err);
-      }
-    });
-  } else {
-    callback([{noData: true}]);
-  }
-};
+
 
 module.exports.getBeerData = getBeerData;
 module.exports.getBeerId = getBeerId;
-
-
